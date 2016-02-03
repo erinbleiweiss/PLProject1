26e28178cb90742ac6eb72564ad95154368245a6
Erin Bleiweiss <erin.bleiweiss@utexas.edu>
2016-02-02 21:23:35 -0600

    initial commit

---
+++ src/bleiweiss/customer/CustomerApp.java
@@ -0,0 +1,10 @@
+package bleiweiss.customer;
+
+public class CustomerApp {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
