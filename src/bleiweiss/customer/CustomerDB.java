0a14896182ad004932dc9604e467237ec1d759df
Allyson Rosenthal <allyson_rosenthal@utexas.edu>
2016-02-02 22:17:49 -0600

    CustomerDB class skeleton

---
+++ src/bleiweiss/customer/CustomerDB.java
@@ -0,0 +1,11 @@
+package bleiweiss.customer;
+
+public class CustomerDB {
+    /* public static String getCustomer(int customerNum) {
+        return Customer(customerNum);
+    }
+
+    if {
+
+    } */
+}
