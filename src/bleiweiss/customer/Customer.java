6e6df384f197bf81c290a5cca2e419dc44ebb41b
Erin Bleiweiss <erin.bleiweiss@utexas.edu>
2016-02-02 21:36:02 -0600

    created customer class

---
+++ src/bleiweiss/customer/Customer.java
@@ -0,0 +1,11 @@
+package bleiweiss.customer;
+
+public class Customer {
+
+    public String name;
+    public String address;
+    public String city;
+    public String state;
+    public String postalCode;
+
+}
