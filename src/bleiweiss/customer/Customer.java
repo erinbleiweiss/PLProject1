77e2c24cce70857b7f743b979f9d9698ea9e8e66
Allyson Rosenthal <allyson_rosenthal@utexas.edu>
2016-02-02 21:54:17 -0600

    getters and setters

--- src/bleiweiss/customer/Customer.java
+++ src/bleiweiss/customer/Customer.java
@@ -2,10 +2,44 @@ package bleiweiss.customer;
 
 public class Customer {
 
-    public String name;
-    public String address;
-    public String city;
-    public String state;
-    public String postalCode;
+    private String name;
+    private String address;
+    private String city;
+    private String state;
+    private String postalCode;
 
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getAddress() {
+        return address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getCity() {
+        return city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+    public String getState() {
+        return state;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+    public String getPostalCode() {
+        return postalCode;
+    }
 }
